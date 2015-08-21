f3d0d96f71fed7253bd188f6e5e80563fc1c6ab6
Liu Yang <liu_yang@worksap.co.jp>
2015-08-21 12:58:09 +0800

    Remove assets.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,12 @@
 package controllers
 
-import play.api._
+import play.api.libs.json._
 import play.api.mvc._
 
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(Json.obj("msg" -> "Hello World"))
   }
 
 }
